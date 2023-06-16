e32bb242da698869935aa2ad87a8e7e50ef976bf
NikolaySlavov-96 <nikolay.slavov.96@gmail.com>
2023-06-16 18:20:00 +0300

    create middleware for session and User Model

---
+++ Exam/28-June - Theaters/middlewares/session.js
@@ -0,0 +1,20 @@
+const { verificationToken } = require("../services/authService");
+
+module.exports = () =>  async (req, res, next) => {
+    const token = req.cookie.token;
+
+    if(token) {
+        try {
+            const userData = await verificationToken(token);
+
+            req.user = userData;
+            res.locals.userName = userData.username;
+        } catch(err) {
+            res.clearCookie('token');
+            res.redirect('/auth/login');
+            return
+        }
+    }
+
+    next();
+}
--- Exam/28-June - Theaters/models/UserModel.js
+++ Exam/28-June - Theaters/models/UserModel.js
@@ -1,3 +1,13 @@
-Username - string (required), unique
-Password - string (required)
-Liked Plays - a collection of Plays 
+const { model, Schema, type: { ObjectId } } = require('mongoose');
+
+const UserSchema = new Schema({
+    username: { type: String, required: true, unique: true, minLength: [3, 'Username is minimal length is 3 letters'] },
+    password: { type: String, required: true },
+    plays: { type: [ObjectId], required: true, default: [], ref: 'Plays' }
+});
+
+UserSchema.index(username, -1);
+
+const UserModel = model('User', UserSchema);
+
+module.exports = UserModel;
